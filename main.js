bb35f621b2dea1fcef5ab764158f461b097e70e0
Eric Denny <103936621+ericdenny4@users.noreply.github.com>
2023-11-01 13:48:18 -0300

    0111

--- main.js
+++ main.js
@@ -2,3 +2,15 @@ function tocaSomPom(){
 document.querySelector('#som_tecla_pom').play();
 }
 const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+while(contador<listaDeTeclas.length){
+   
+    listaDeTeclas[0].onclick = tocaSomPom;
+    contador = contador + 1;
+
+
+    console.log(contador);
+
+}
