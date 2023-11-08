43232d9b94dc30324b0b758fc4932a0b34e85c83
Eric Denny <103936621+ericdenny4@users.noreply.github.com>
2023-11-08 13:53:52 -0300

    0811

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-function tocaSomPom(){
-document.querySelector('#som_tecla_pom').play();
+function tocaSom(idElementoAudio){
+
+document.querySelector(idElementoAudio).play();
 }
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
@@ -7,7 +8,9 @@ let contador = 0;
 
 while(contador<listaDeTeclas.length){
    
-    listaDeTeclas[0].onclick = tocaSomPom;
+    listaDeTeclas[contador].onclick = function(){
+        tocaSom('#som_tecla_splash')
+    };
     contador = contador + 1;
 
 
