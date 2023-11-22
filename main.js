97560cddeba5d9c829a5f1d3087789c44469ef18
Eric Denny <103936621+ericdenny4@users.noreply.github.com>
2023-11-22 14:08:48 -0300

    2211

--- main.js
+++ main.js
@@ -4,16 +4,19 @@ document.querySelector(idElementoAudio).play();
 }
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
 
-while(contador<listaDeTeclas.length){
+for (let contador = 0;contador<listaDeTeclas.length; contador++){
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
    
-    listaDeTeclas[contador].onclick = function(){
-        tocaSom('#som_tecla_splash')
+    tecla.onclick = function(){
+        tocaSom(idAudio)
     };
-    contador = contador + 1;
 
-
-    console.log(contador);
+    tecla.onkeydown = function(){
+        tecla.classList.add('ativa');
+    }
 
 }
